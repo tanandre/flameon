f37481dbfadfc63c6f6a632665c17ecc4baa38a2
tanandre <tanneman1@gmail.com>
2021-11-26 12:17:39 -0500

    adding patient component to query fhir api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flameon';
+  ngOnInit(): void {
+    console.log('test');
+  }
 }
---
+++ src/app/patients/patients.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+interface Bundle {
+  entry: Entry[]
+}
+
+interface Entry {
+  resource: Patient
+}
+
+interface Patient {
+  id: string;
+  resourceType: string;
+}
+
+@Component({
+  selector: 'app-patients',
+  templateUrl: './patients.component.html',
+  styleUrls: ['./patients.component.css']
+})
+
+
+export class PatientsComponent implements OnInit {
+  data: any;
+  response: Observable<Bundle>;
+
+  constructor(private client: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    console.log(this);
+
+    this.response = this.client.get<Bundle>("https://hapi.fhir.org/baseR4/Patient", {});
+  }
+
+}
